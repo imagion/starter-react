106916d6e801dc2fb3fc9a25acd139a4c6e4c8f0
imagion <algernoncowperwood@gmail.com>
2022-04-23 20:29:09 +0300

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
 // pages and components
-import Header from './components/Header'
 import Home from './pages/Home'
 
 export default function App() {
   return (
     <>
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
