b67cbcc25257ce0feba7b3d8482d36a531dfd305
imagion <algernoncowperwood@gmail.com>
2022-07-15 17:30:33 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // pages and components
 import Home from './pages/Home'
-import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
